48a43abbcc2c37a17442072eceeb5ef5e7c6aa2f
bijoy133 <bijoydas5680@!gmail.com>
2025-04-24 18:44:48 +0600

    added a manual chunk

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000, 
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'react'
+            if (id.includes('chart.js')) return 'chart'
+            return 'vendor'
+          }
+        }
+      }
+    }
+  }
 })
